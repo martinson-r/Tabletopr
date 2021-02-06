67bbd294bc88774c6fdd0b40027cf5d077b87b5f
Rihana Chang <martinson.r@gmail.com>
2021-02-05 17:27:01 -0800

    troubleshoot ssl issue

--- backend/config/database.js
+++ backend/config/database.js
@@ -18,9 +18,6 @@ module.exports = {
   production: {
     use_env_variable: "DATABASE_URL",
     dialect: "postgres",
-    dialectOptions: {
-      "ssl": {"require":true }
-    },
     seederStorage: "sequelize",
   },
 };
