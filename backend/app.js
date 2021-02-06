bede154996439c905e6403db92aec9ab7c68671c
Rihana Chang <martinson.r@gmail.com>
2021-02-05 18:08:58 -0800

    troubleshoot https issue

--- backend/app.js
+++ backend/app.js
@@ -17,6 +17,17 @@ app.use(morgan('dev'));
 app.use(cookieParser());
 app.use(express.json());
 
+
+function requireHTTPS(req, res, next) {
+  // The 'x-forwarded-proto' check is for Heroku
+  if (!req.secure && req.get('x-forwarded-proto') !== 'https' && process.env.NODE_ENV !== "development") {
+    return res.redirect('https://' + req.get('host') + req.url);
+  }
+  next();
+}
+
+app.use(requireHTTPS);
+
 // Security Middleware
 if (!isProduction) {
   // enable cors only in development
