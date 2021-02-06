5d75feb9961ab56858cbd1b323bffaf99f57b4ae
Rihana Chang <martinson.r@gmail.com>
2021-02-05 16:39:37 -0800

    Add sslmode require

--- backend/config/database.js
+++ backend/config/database.js
@@ -16,6 +16,7 @@ module.exports = {
     seederStorage: "sequelize",
   },
   production: {
+    sslmode: "require",
     use_env_variable: "DATABASE_URL",
     dialect: "postgres",
     seederStorage: "sequelize",
